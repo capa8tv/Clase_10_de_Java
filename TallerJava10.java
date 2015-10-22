d1149e98190a08d32f9b9395d97b63c52ff97464
Capa8.tv <canalcapa8@gmail.com>
2015-10-21 23:06:13 -0400

    TallerJava10

--- TallerJava10.java
+++ TallerJava10.java
@@ -3,7 +3,7 @@ package estudioeventos;
 
 import graficas.Marco1;
 
-public class EstudioEventos {
+public class TallerJava10 {
     public static void main(String[] args) 
     {
         Marco1 marco=new Marco1();
