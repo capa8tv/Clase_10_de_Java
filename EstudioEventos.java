24d58de0c0829761b4cd6dafa6c6145f8f29f15d
Capa8.tv <canalcapa8@gmail.com>
2015-10-21 23:05:14 -0400

    EstudioEventos.java

---
+++ EstudioEventos.java
@@ -0,0 +1,13 @@
+
+package estudioeventos;
+
+import graficas.Marco1;
+
+public class EstudioEventos {
+    public static void main(String[] args) 
+    {
+        Marco1 marco=new Marco1();
+        marco.getAppCentrada();
+    }
+    
+}
